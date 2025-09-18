b1ee8bbfc6e4e4a7de1fe594cef92d16f7842ac7
chaso-pa <chaso.party.area@gmail.com>
2025-09-19 06:30:51 +0900

    add: utils

---
+++ internal/utils/env.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	// 読み込めなかったら err にエラーが入ります。
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("読み込み出来ませんでした: %v", err)
+	}
+}
+
